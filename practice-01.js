b2c41c4adf23c283741e81e982db471f9606ab6f
spaceC00kie <kirstie317@gmail.com>
2020-11-25 22:43:28 -0500

    practicing

---
+++ practice-01.js
@@ -0,0 +1,8 @@
+//objective is to loop 0 throughout 10
+
+for (i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+const shoes = ["sneakers", "sandals", "boots", "flip flops", "barefoot"];
+shoes.forEach((shoe) => console.log(shoe));
